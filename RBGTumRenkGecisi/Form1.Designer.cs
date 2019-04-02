2b8a4a4635d976994f773052b6b789d422e60d90
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2019-04-02 12:26:34 +0300

    Add files via upload

---
+++ RBGTumRenkGecisi/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RBGTumRenkGecisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(784, 361);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB Renk Geçişleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RBGTumRenkGecisi/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBGTumRenkGecisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.BackColor = Color.FromArgb(R,G,B);
+            label1.Text = "R:" + R + " G:" + G + " B:" + B;
+        }
+
+        int R = 255, G = 0, B = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (R==255 && G==0 && B<=255)
+            {
+                if (B<255)
+                {
+                    B++;
+                }
+            }
+            if (R>=0 && G==0 && B==255)
+            {
+                if (R>0)
+                {
+                    R--;
+                }
+            }
+            if (R==0 && G<=255 && B==255)
+            {
+                if (G<255)
+                {
+                    G++;
+                }
+            }
+            if (R==0 && G==255 && B>=0)
+            {
+                if (B>0)
+                {
+                    B--;
+                }
+            }
+
+            if (R<=255 && G == 255 && B==0)
+            {
+                if (R<255)
+                {
+                    R++;
+                }
+            }
+            if (R==255 && G>=0 && B>=0)
+            {
+                if (G>0)
+                {
+                    G--;
+                }
+            }
+            this.BackColor=Color.FromArgb(R,G,B);
+           label1.Text = "R:" + R + " G:" + G + " B:" + B;
+        
+        }
+    }
+}
